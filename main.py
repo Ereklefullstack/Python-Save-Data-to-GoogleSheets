44d8ee010e7b0f3603430148e70944a1e0dde4e1
Erekle Shindagoridze <130274791+Ereklefullstack@users.noreply.github.com>
2024-10-30 08:41:50 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,9 @@ from google.oauth2.service_account import Credentials
 creds = Credentials.from_service_account_file('path/to/your/credentials.json')
 client = gspread.authorize(creds)
 
-# Open Google sheet using its name or URL
 spreadsheet = client.open("Your Google Sheet Name")
 
-# Select the desired sheet
 worksheet = spreadsheet.sheet1  # Or you can use the name of the sheet: spreadsheet.worksheet('Sheet1')
 
-# Add a new row
 row = ["example@example.com", "valid"]  # Here you can enter the information you want
 worksheet.append_row(row)
